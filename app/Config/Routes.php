fef5144cd59c92df93f1dd9b54f20ab553acdfe6
manuu <manuelurrutia118@gmail.com>
2026-04-01 22:22:36 -0300

    dsfsf

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,5 +12,5 @@ $routes->get('/', 'Home::index');
 $routes->get('clientes', 'Clientes::index');
 $routes->get('clientes/historial_pedidos/(:num)', 'Clientes::historial_pedidos/$1');
 $routes->get('clientes/historial_cuenta/(:num)', 'Clientes::historial_cuenta/$1');
-$routes->get('clientes/nuevo_pedido/(:num)', 'Clientes::nuevo_pedido/$1');
+
 $routes->get('clientes/nuevo_pedido/(:num)', 'Pedidos::nuevo/$1');
