60d96c80c2b159e99aba09a022def11a057dbcaf
manuu <manuelurrutia118@gmail.com>
2026-04-02 17:59:27 -0300

    fdgdfg

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -15,7 +15,7 @@ $routes->get('clientes/historial_cuenta/(:num)', 'Clientes::historial_cuenta/$1'
 
 $routes->get('pedidos/productos/(:num)/(:any)', 'Pedidos::productos/$1/$2');
 $routes->get('clientes/nuevo_pedido/(:num)', 'Pedidos::nuevo/$1');
-$routes->post('admin/marcas', 'Pedidos::guardar_categoria');
+$routes->post('pedidos/guardar_categoria', 'Pedidos::guardar_categoria');
 $routes->get('pedidos/borrar_categoria/(:num)/(:num)', 'Pedidos::borrar_categoria/$1/$2');
 // Rutas de Administración
 $routes->get('admin', 'Admin::index');
